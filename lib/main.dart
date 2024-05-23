224e382a8a4b6da39f4ba344cbfdfecb97e5ba03
yuminseo <sun-yms@naver.com>
2024-05-23 12:32:28 +0900

    홈화면 UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          color: Color(0xFFFFDE97), // FFDE97 색상의 배경
+          child: Center(
+            child: Container(
+              width: 200, // 원의 너비
+              height: 200, // 원의 높이
+              decoration: BoxDecoration(
+                color: Color(0xFFFF901C), // FF901C 색상의 동그라미
+                shape: BoxShape.circle, // 동그라미 모양
+              ),
+            ),
+          ),
         ),
       ),
     );
